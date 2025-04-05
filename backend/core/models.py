a8a8bee1ed62b221bfc23f34c067a0926473cb11
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-05 19:48:36 +0300

    Create Article model

--- backend/core/models.py
+++ backend/core/models.py
@@ -21,6 +21,14 @@ def project_image_path(instance: "ProjectImage", filename: str) -> pathlib.Path:
     return pathlib.Path("upload/projects/gallery_image/") / new_filename
 
 
+def post_image_path(article: "Article", filename: str) -> pathlib.Path:
+    filename_suffix = pathlib.Path(filename).suffix
+    unique_id = uuid.uuid4()
+    slug_name = slugify(article.title)
+    new_file_name = f"{slug_name}-{unique_id}{filename_suffix}"
+    return pathlib.Path("upload/articles/") / new_file_name
+
+
 class Tag(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
@@ -91,3 +99,16 @@ class ProjectConfiguration(models.Model):
 
     def __str__(self) -> str:
         return f"{self.name} ({self.price} UAH)"
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to=post_image_path)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return self.title
