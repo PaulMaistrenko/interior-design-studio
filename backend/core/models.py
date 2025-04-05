9338952c830cc425deda22e190bbfcb8660a8105
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-05 16:57:34 +0300

    Create ProjectStyle model

--- backend/core/models.py
+++ backend/core/models.py
@@ -9,3 +9,13 @@ class Tag(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class ProjectStyle(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
