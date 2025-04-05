688126e785a72fb3b67de6d82be8cf56cb1bbc3c
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-05 17:07:52 +0300

    Create Project model and add media files configuration

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,4 +1,16 @@
+import pathlib
+import uuid
+
 from django.db import models
+from django.utils.text import slugify
+
+
+def project_main_image_path(project: "Project", filename: str) -> pathlib.Path:
+    filename_suffix = pathlib.Path(filename).suffix
+    unique_id = uuid.uuid4()
+    slug_name = slugify(project.name)
+    new_filename = f"{slug_name}-{unique_id}{filename_suffix}"
+    return pathlib.Path("upload/projects/main_image/") / new_filename
 
 
 class Tag(models.Model):
@@ -16,6 +28,24 @@ class ProjectStyle(models.Model):
 
     class Meta:
         ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    style = models.ForeignKey(
+        ProjectStyle,
+        on_delete=models.CASCADE,
+        related_name="projects"
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="projects"
+    )
+    main_image = models.ImageField(upload_to=project_main_image_path)
 
     def __str__(self) -> str:
         return self.name
