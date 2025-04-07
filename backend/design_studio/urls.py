ce78bd7e50211a012031379efbdb970b352a0f3f
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-07 17:17:56 +0300

    Add django-debug-toolbar configurations

--- backend/design_studio/urls.py
+++ backend/design_studio/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/core/", include("core.urls", namespace="core")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
