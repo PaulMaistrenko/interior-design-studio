6cd6c49dbdcf635ba0cfbfc6c3037449b9f23088
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-08 15:43:20 +0300

    Add ArticleSerializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
+from babel.dates import format_datetime
+from django.utils import timezone
 
-from core.models import Tag, ProjectStyle, Project, ProjectConfiguration
+from core.models import (
+    Tag,
+    ProjectStyle,
+    Project,
+    ProjectConfiguration,
+    Article,
+)
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -57,3 +65,18 @@ class ProjectConfigurationSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectConfiguration
         fields = ("id", "name", "price", "services")
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    created_at = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Article
+        fields = ("id", "title", "content", "image", "created_at")
+
+    def get_created_at(self, obj):
+        local_time = timezone.localtime(obj.created_at)
+        formatted = format_datetime(local_time, "dd MMM y", locale="uk")
+        day, month, year = formatted.split(" ")
+        month = month.capitalize()
+        return f"{day} {month} {year}"
