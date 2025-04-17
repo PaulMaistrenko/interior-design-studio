c21eb3254d4bfe7a21d485409320e16e9928c207
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-17 18:45:32 +0300

    Move Article model to blog app

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import pathlib
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+def post_image_path(article: "Article", filename: str) -> pathlib.Path:
+    filename_suffix = pathlib.Path(filename).suffix
+    unique_id = uuid.uuid4()
+    slug_name = slugify(article.title)
+    new_file_name = f"{slug_name}-{unique_id}{filename_suffix}"
+    return pathlib.Path("upload/articles/") / new_file_name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to=post_image_path)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return self.title
