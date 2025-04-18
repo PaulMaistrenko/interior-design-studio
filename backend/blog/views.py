6aa6968dd16935e97984fd319ee72f8bd2960bf1
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-04-18 16:10:03 +0300

    Add ArticleDetailView with url mapping

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -2,7 +2,7 @@ from drf_spectacular.utils import extend_schema
 from rest_framework import generics
 
 from blog.models import Article
-from blog.serializers import ArticleSerializer
+from blog.serializers import ArticleListSerializer, ArticleDetailSerializer
 
 
 @extend_schema(
@@ -10,8 +10,15 @@ from blog.serializers import ArticleSerializer
     description="Returns a list of articles about design trends. "
                 "Each article includes its title, content, image, "
                 "and creation date.",
-    responses=ArticleSerializer
+    responses=ArticleListSerializer
 )
 class ArticleListView(generics.ListAPIView):
     queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+    serializer_class = ArticleListSerializer
+
+
+class ArticleDetailView(generics.RetrieveAPIView):
+    queryset = Article.objects.prefetch_related(
+        "components__features", "components__advantages"
+    )
+    serializer_class = ArticleDetailSerializer
