275c730bed309a83ca0772c66517fccd5c2f0964
Pavel Maistrenko <maystrenko.p@gmail.com>
2025-04-25 20:54:54 +0200

    create hero section of main page

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,65 +5,63 @@ import './Header';
 export const Header = () => {
   return (
     <header className="header">
-      <div className="header__top">
-        <div className="container">
-          <div className="header__actions">
-            <MainLogo className={'main-logo--header'} />
-            <nav className="header__nav nav">
-              <ul className="nav__list">
-                <li className="nav__item">
-                  <NavLink
-                    to="/"
-                    className={({ isActive }) =>
-                      isActive ? 'nav__link nav__link--active' : 'nav__link'
-                    }
-                  >
-                    Головна
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink
-                    to="/services"
-                    className={({ isActive }) =>
-                      isActive ? 'nav__link nav__link--active' : 'nav__link'
-                    }
-                  >
-                    Послуги
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink
-                    to="/projects"
-                    className={({ isActive }) =>
-                      isActive ? 'nav__link nav__link--active' : 'nav__link'
-                    }
-                  >
-                    Портфоліо
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink
-                    to="/blog"
-                    className={({ isActive }) =>
-                      isActive ? 'nav__link nav__link--active' : 'nav__link'
-                    }
-                  >
-                    Блог
-                  </NavLink>
-                </li>
-                <li className="nav__item">
-                  <NavLink
-                    to="/contacts"
-                    className={({ isActive }) =>
-                      isActive ? 'nav__link nav__link--active' : 'nav__link'
-                    }
-                  >
-                    Контакти
-                  </NavLink>
-                </li>
-              </ul>
-            </nav>
-          </div>
+      <div className="container">
+        <div className="header__content">
+          <MainLogo className={'main-logo--header'} />
+          <nav className="header__nav nav">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? 'nav__link nav__link--active' : 'nav__link'
+                  }
+                >
+                  Головна
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink
+                  to="/services"
+                  className={({ isActive }) =>
+                    isActive ? 'nav__link nav__link--active' : 'nav__link'
+                  }
+                >
+                  Послуги
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink
+                  to="/projects"
+                  className={({ isActive }) =>
+                    isActive ? 'nav__link nav__link--active' : 'nav__link'
+                  }
+                >
+                  Портфоліо
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink
+                  to="/blog"
+                  className={({ isActive }) =>
+                    isActive ? 'nav__link nav__link--active' : 'nav__link'
+                  }
+                >
+                  Блог
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink
+                  to="/contacts"
+                  className={({ isActive }) =>
+                    isActive ? 'nav__link nav__link--active' : 'nav__link'
+                  }
+                >
+                  Контакти
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
---
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import './MainPage';
+
+export const MainPage = () => {
+  return (
+    <div className="page main-page">
+      <section className="hero-section bg-image">
+        <div className="container">
+          <div className="hero-section__title">
+            МИ СТВОРЮЄМО ІНТЕР’ЄРИ,
+            <br /> В ЯКИХ ХОЧЕТЬСЯ ЖИТИ
+          </div>
+          <Link
+            to="/contacts"
+            className="hero-section__button button button--text"
+          >
+            Зв’язатися з нами
+          </Link>
+        </div>
+      </section>
+    </div>
+  );
+};
