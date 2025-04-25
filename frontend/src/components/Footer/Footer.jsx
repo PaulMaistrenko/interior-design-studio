c113f21a889fe2018953067c1cc44c5f2d7495d1
Pavel Maistrenko <maystrenko.p@gmail.com>
2025-04-25 21:47:24 +0200

    created footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,38 @@
+import { MainLogo } from '../UI/MainLogo';
+//import './Footer';
+
 export const Footer = () => {
   return (
-    <footer className="footer">Footer</footer>
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__content">
+          <MainLogo className={'main-logo--footer'} />
+          <nav className="footer__contacts">
+            <a
+              href="mailto:tavrdesignhouse@gmail.com"
+              className="footer__contact email"
+            >
+              tavrdesignhouse@gmail.com
+            </a>
+            <p>|</p>
+            <a href="tel:+380931389963" className="footer__contact mobile">
+              +38 093 138 99 63
+            </a>
+            <p>|</p>
+            <div className="footer__socials">
+              <a href="/" className="footer__social footer__social--instagram">
+                instagram
+              </a>
+              <a href="/" className="footer__social footer__social--telegram">
+                telegram
+              </a>
+              <a href="/" className="footer__social footer__social--viber">
+                viber
+              </a>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </footer>
   );
-}
+};
