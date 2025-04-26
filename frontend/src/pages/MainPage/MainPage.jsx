c559ae76e3661d90c5145d51dd47d3e9290d0aea
Pavel Maistrenko <maystrenko.p@gmail.com>
2025-04-26 17:59:17 +0200

    created grid file

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export const MainPage = () => {
   return (
     <div className="page main-page">
-      <section className="hero-section bg-image">
+      <section className="hero bg-image">
         <div className="container">
           <div className="hero-section__title">
             МИ СТВОРЮЄМО ІНТЕР’ЄРИ,
@@ -15,6 +15,20 @@ export const MainPage = () => {
           >
             Зв’язатися з нами
           </Link>
+        </div>
+      </section>
+      <section className="about">
+        <div className="container">
+          <div className="about__top">
+            <h4 className="about__title">Про нас</h4>
+            <h1 className="about__tagline">“Наші корені — наша сила”</h1>
+          </div>
+          <div className="about__content">
+            <div className="about__subtitle"></div>
+            <div className="about__text"></div>
+            <div className="about__poster-1"></div>
+            <div className="about__poster-2"></div>
+          </div>
         </div>
       </section>
     </div>
