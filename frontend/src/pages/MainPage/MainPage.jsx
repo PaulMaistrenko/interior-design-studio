b6132ea18556cf58d47082661535f68240f1a3da
Pavel Maistrenko <maystrenko.p@gmail.com>
2025-04-27 13:22:27 +0200

    block about us is completed

--- frontend/src/pages/MainPage/MainPage.jsx
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -20,14 +20,24 @@ export const MainPage = () => {
       <section className="about">
         <div className="container">
           <div className="about__top">
-            <h4 className="about__title">Про нас</h4>
-            <h1 className="about__tagline">“Наші корені — наша сила”</h1>
+            <h4 className="about__title h4--regular">Про нас</h4>
+            <h1 className="about__tagline h1--bold">
+              “Наші корені — наша сила”
+            </h1>
           </div>
-          <div className="about__content">
-            <div className="about__subtitle"></div>
-            <div className="about__text"></div>
-            <div className="about__poster-1"></div>
-            <div className="about__poster-2"></div>
+          <div className="about__content grid">
+            <div className="about__subtitle grid--design-5-12 h4--bold">
+              Tavr Design House — це не просто студія інтер’єру. Це історія,
+              вкорінена в землі Таврії — давньої назви нашого рідного Херсона
+            </div>
+            <div className="about__text grid--design-4-8 ">
+              У своїй роботі ми поєднуємо архітектурну міцність і візуальну
+              виразність. Надихаючись символом таврового профілю —
+              конструктивного елементу, що уособлює витривалість і практичність,
+              — ми створюємо інтер’єри, які служать роками.
+            </div>
+            <div className="about__poster-1 grid--design-1-3 bg-image"></div>
+            <div className="about__poster-2 grid--design-9-12 bg-image"></div>
           </div>
         </div>
       </section>
