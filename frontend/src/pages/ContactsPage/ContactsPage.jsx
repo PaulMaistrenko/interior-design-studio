37dca67fbedb6a04e7420a621b8a5867044bd1b5
Pavel Maistrenko <maystrenko.p@gmail.com>
2025-04-28 22:44:14 +0200

    added contacts page

--- frontend/src/pages/ContactsPage/ContactsPage.jsx
+++ frontend/src/pages/ContactsPage/ContactsPage.jsx
@@ -9,14 +9,14 @@ export const ContactsPage = () => {
       <div className="container">
         <Breadcrumb currentPage={currentPage} />
         <div className="contacts-page__content grid">
-          <h1 className="contacts-page__title h1--bold grid--onDesktop-2-12">
+          <h1 className="contacts-page__title h1--bold grid--onDesktop-2-11">
             Кожен простір має свою історію. Давай напишемо твою разом.
           </h1>
-          <h4 className="contacts-page__text h4--regular grid--onDesktop-2-12">
+          <h4 className="contacts-page__text h4--regular grid--onDesktop-2-11">
             Залиши повідомлення , і ми зв’яжемося з тобою, щоб обговорити
             деталі.
           </h4>
-          <div className="contact-form__wrapper grid--onDesktop-2-12">
+          <div className="contact-form__wrapper grid--onDesktop-2-11">
             <ContactForm />
           </div>
         </div>
