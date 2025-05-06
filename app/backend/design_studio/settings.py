f16bf8b0d658809e778b312bca3a47ddd2e49bbf
88victory88 <victory.otrok@gmail.com>
2025-05-06 16:52:54 +0800

    CI/CD 1

--- app/backend/design_studio/settings.py
+++ app/backend/design_studio/settings.py
@@ -30,9 +30,11 @@ SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-# ALLOWED_HOSTS = ["*", "172.166.90.117"] if DEBUG else ["localhost", "127.0.0.1"]
+# ALLOWED_HOSTS = ["*"] if DEBUG else ["localhost", "127.0.0.1"]
 # ALLOWED_HOSTS = ["172.166.90.117", "localhost", "127.0.0.1"]
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
+FORCE_SCRIPT_NAME = os.getenv("FORCE_SCRIPT_NAME", "")
+# FORCE_SCRIPT_NAME = ""
 
 
 # Application definition
